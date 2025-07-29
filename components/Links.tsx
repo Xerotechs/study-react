5b47ea6e670eee7ad05a00e5842dd3afc784bf3b
Xerotecs <fujinokeito@icloud.com>
2025-07-29 21:58:23 +0900

    LinksのmoduleCSSを修正

--- components/Links.tsx
+++ components/Links.tsx
@@ -1,31 +1,32 @@
 import Image from "next/image";
+import styles from "../src/app/page.module.css";
 
 export function Links(){
     return(
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-            <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:w-auto"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
+        <div className={styles.ctas}>
+          <a
+            className={styles.primary}
+            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
             target="_blank"
             rel="noopener noreferrer"
-            >
+          >
             <Image
-                className="dark:invert"
-                src="/vercel.svg"
-                alt="Vercel logomark"
-                width={20}
-                height={20}
+              className={styles.logo}
+              src="/vercel.svg"
+              alt="Vercel logomark"
+              width={20}
+              height={20}
             />
             デプロイ
-            </a>
-            <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 w-full sm:w-auto md:w-[158px]"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
+          </a>
+          <a
+            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
             target="_blank"
             rel="noopener noreferrer"
-            >
+            className={styles.secondary}
+          >
             ドキュメント
-            </a>
+          </a>
         </div>
     );
 }
--- components/Main.tsx
+++ components/Main.tsx
@@ -1,6 +1,7 @@
 import Image from "next/image";
-import { Headline } from "./Headline";
 import styles from "../src/app/page.module.css";
+import { Headline } from "./Headline";
+import { Links } from "./Links";
 
 interface  Props{
     page:string
@@ -25,31 +26,7 @@ export function Main({page}:Props){
                 </li>
             </Headline>
 
-        <div className={styles.ctas}>
-          <a
-            className={styles.primary}
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className={styles.logo}
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-            className={styles.secondary}
-          >
-            Read our docs
-          </a>
-        </div>
+        <Links />
       </main>
     );
 }
