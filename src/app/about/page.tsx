24b7ef6f3bd108b47ab1d0740d25159c4534d036
Xerotecs <fujinokeito@icloud.com>
2025-08-04 22:55:42 +0900

    ページ情報追加

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -2,13 +2,20 @@ import styles from "@/page/page.module.css";
 import { Footer } from "@/components/Footer";
 import { Main } from "@/components/Main";
 import { Header } from "@/components/Header";
+import type { Metadata } from "next";
+
+// このページ専用の metadata を定義
+export const metadata: Metadata = {
+  title: "About",
+  description: "About"
+};
 
 export default function Home() {
   return (
-      <div className={styles.page}>
-        <Header/>
-        <Main page={"about"}/>
-        <Footer/>
+    <div className={styles.page}>
+      <Header/>
+      <Main page={"about"}/>
+      <Footer/>
     </div>
   );
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,6 +2,13 @@ import styles from "@/page/page.module.css";
 import { Footer } from "@/components/Footer";
 import { Main } from "@/components/Main";
 import { Header } from "@/components/Header";
+import type { Metadata } from "next";
+
+// このページ専用の metadata を定義
+export const metadata: Metadata = {
+  title: "Page",
+  description: "Page"
+};
 
 export default function Home() {
   return (
